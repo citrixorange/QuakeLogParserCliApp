328fdd020c23123f1758ad6f981665f73fa24e87
cesar-sandbox <cesar-sandbox@ubuntu.myguest.virtualbox.org>
2024-02-21 22:09:36 -0300

    Supporting new config args

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ extern crate quake_log_parser_lib;
 
 use quake_log_parser_lib::{
     errors::LogParserError,
-    config::config::{CONFIG_FILE_PATH},
-    implementation::log_parser::LOG_FILE_PATH,
+    config::config::{ConfigValue},
+    config::dynamic_config::{CONFIG_FILE_PATH, CONFIG, ConfigParameter},
     interface::CallbackPayload,
     lib::factory
 };
@@ -16,13 +16,22 @@ use chrono::Utc;
 
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(author="citrixorange", version, about="A cli app for parsing local Quake log files", long_about = None)]
 struct Args {
-    #[arg(short, long)]
+    #[arg(short, long, help="path for config .json file")]
     config_file: String,
 
-    #[arg(short, long)]
+    #[arg(short, long, help="path for log .log file")]
     log_file: String,
+
+    #[arg(short, long, help="include kill means in match stats")]
+    enable_kill_by_means: Option<bool>,
+
+    #[arg(short, long, help="if true, self kill increases player kill score")]
+    self_kill_score_increase: Option<bool>,
+
+    #[arg(short, long, help="if true, being killed decreases player kill score")]
+    being_killed_decreases_score: Option<bool>,
 }
 
 pub async fn success_callback(success_data: Option<Value>) -> Result<(), LogParserError> {
@@ -83,7 +92,7 @@ pub async fn warning_callback(warning_data: Option<Value>) -> Result<(), LogPars
 #[tokio::main]
 async fn main() {
 
-    env_logger::from_env(Env::default().default_filter_or("info")).init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     
     let args = Args::parse();
 
@@ -91,8 +100,22 @@ async fn main() {
         *config_file_path_handler.borrow_mut() = Some(args.config_file);
     });
 
-    LOG_FILE_PATH.with(|log_file_path_handler| {
-        *log_file_path_handler.borrow_mut() = Some(args.log_file);
+    CONFIG.with(|config| {
+
+        config.borrow_mut().set_parameter(ConfigParameter::LogFilePath, ConfigValue::Str(args.log_file));
+
+        if let Some(show_kill_by_means) = args.enable_kill_by_means {
+            config.borrow_mut().set_parameter(ConfigParameter::ShowDeathCauses, ConfigValue::Bool(show_kill_by_means));
+        }
+    
+        if let Some(self_kill_score_increase) = args.self_kill_score_increase {
+            config.borrow_mut().set_parameter(ConfigParameter::KillYourselfIncreasesScore, ConfigValue::Bool(self_kill_score_increase));
+        }
+    
+        if let Some(being_killed_decreases_score) = args.being_killed_decreases_score {
+            config.borrow_mut().set_parameter(ConfigParameter::BeingKilledDecreasesScore, ConfigValue::Bool(being_killed_decreases_score));
+        }
+
     });
     
     let mut log_parser = factory();
